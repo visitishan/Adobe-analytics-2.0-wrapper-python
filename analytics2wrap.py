8ac699cc005f118784aacccba53fd8567da68884
Ishan Jain <visitishan@gmail.com>
2019-04-27 18:31:41 +0530

    initial commit
    - created wrapper file

---
+++ analytics2wrap.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 25 13:16:00 2019
+
+@author: ishan.m.jain
+"""
+
+import requests
+import json
+import pandas as pd
+from time import sleep
+
+
+
+
+headers={'Authorization':'Bearer aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+'x-proxy-global-company-id':'bbbbbbbbbbbbbb',
+'x-api-key':'cccccccccccccccccccccccccccccccccc',
+'Accept': 'application/json',
+'Content-Type': 'application/json'}
+
+reporturl = "https://analytics.adobe.io/api/bbbbbbbbbbbbbbbbbb/reports"
+
