0a2d022beee205c573d6dcb5ebc88377fbfdc053
Ishan Jain <visitishan@gmail.com>
2019-05-07 21:24:41 +0530

    few changes

--- analytics2wrap.py
+++ analytics2wrap.py
@@ -10,11 +10,18 @@ import pandas as pd
 from time import sleep
 
 
-headers, auth_token, company_id, reporturl, y, y2, date_range = 0
+#headers, auth_token, company_id, reporturl, y, y2
+dimentionlist = []
+dataframelist = []
 
+
+#####################################################################
+#	To create connection to the API. 
+#	Call with auth token, company id, and client id in parameters
+#####################################################################
 def create_con(auth_token,company_id,client_id):
-	global headers
-	headers={'Authorization':'Bearer '+auth_token+'',
+	#global headers
+	headers={'Authorization': auth_token,
 	'x-proxy-global-company-id':''+company_id +'',
 	'x-api-key':''+client_id+'',
 	'Accept': 'application/json',
@@ -24,7 +31,6 @@ def create_con(auth_token,company_id,client_id):
 	reporturl = "https://analytics.adobe.io/api/"+company_id+"/reports"
 
 
-
 def find_id(response):
    dim = json.loads(response)
    rows = dim['rows']
@@ -32,12 +38,19 @@ def find_id(response):
    return result_dict
 
 
-
-def get_item_id(element, date_range):
-	global date_range
+#####################################################################
+#	To get item id of a dimention 
+#	Call with dimention name and date range in parameters
+#	Date range format = 2019-03-01T00:00:00.000/2019-04-01T00:00:00.000
+#	The function returns the dimention and item ids in a dictionary
+#####################################################################
+def get_item_id(element, date_range, company_id):
+	#global date_range
 	element = '{"rsid":"'+company_id+'","globalFilters":[{"type":"dateRange","dateRange":"'+date_range+'"}],"metricContainer":{"metrics":[{"columnId":"0","id":"metrics/occurrences","sort":"desc"}]},"dimension":"variables/'+element+'","settings":{"countRepeatInstances":"true","limit":10000,"page":0},"statistics":{"functions":["col-max","col-min"]}}'
+	print(element)
 	y2 = json.dumps(element)
 	element2  = requests.post(url = reporturl, headers = headers, data = element).text
 	element2 = find_id(element2)
+	dimentionlist.append(element2)
 	return element2
 
