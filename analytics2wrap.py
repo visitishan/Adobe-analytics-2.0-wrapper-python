b682e907427554ed2ee5aaffda0491b6501d9d23
Ishan Jain <visitishan@gmail.com>
2019-05-10 23:21:24 +0530

    Updated functions
    Create_con and Get_id functions are working fine now

--- analytics2wrap.py
+++ analytics2wrap.py
@@ -20,15 +20,16 @@ dataframelist = []
 #	Call with auth token, company id, and client id in parameters
 #####################################################################
 def create_con(auth_token,company_id,client_id):
-	#global headers
-	headers={'Authorization': auth_token,
-	'x-proxy-global-company-id':''+company_id +'',
-	'x-api-key':''+client_id+'',
-	'Accept': 'application/json',
-	'Content-Type': 'application/json'}
-	y = json.dumps(headers)
-	global reporturl
-	reporturl = "https://analytics.adobe.io/api/"+company_id+"/reports"
+    global headers
+    headers={'Authorization': auth_token,
+    'x-proxy-global-company-id':''+company_id +'',
+    'x-api-key':''+client_id+'',
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'}
+    y = json.dumps(headers)
+    global reporturl
+    reporturl = 'https://analytics.adobe.io/api/'+company_id+'/reports'
+    return headers, reporturl
 
 
 def find_id(response):
@@ -44,13 +45,14 @@ def find_id(response):
 #	Date range format = 2019-03-01T00:00:00.000/2019-04-01T00:00:00.000
 #	The function returns the dimention and item ids in a dictionary
 #####################################################################
-def get_item_id(element, date_range, company_id):
-	#global date_range
-	element = '{"rsid":"'+company_id+'","globalFilters":[{"type":"dateRange","dateRange":"'+date_range+'"}],"metricContainer":{"metrics":[{"columnId":"0","id":"metrics/occurrences","sort":"desc"}]},"dimension":"variables/'+element+'","settings":{"countRepeatInstances":"true","limit":10000,"page":0},"statistics":{"functions":["col-max","col-min"]}}'
-	print(element)
-	y2 = json.dumps(element)
-	element2  = requests.post(url = reporturl, headers = headers, data = element).text
-	element2 = find_id(element2)
-	dimentionlist.append(element2)
-	return element2
+def get_item_id(element, date_range, global_company_id):
+    element = '{"rsid":"'+global_company_id+'","globalFilters":[{"type":"dateRange","dateRange":"'+date_range+'"}],"metricContainer":{"metrics":[{"columnId":"0","id":"metrics/occurrences","sort":"desc"}]},"dimension":"variables/'+element+'","settings":{"countRepeatInstances":"true","limit":10000,"page":0},"statistics":{"functions":["col-max","col-min"]}}'
+    #print(element)
+    y2 = json.dumps(element)
+    element2  = requests.post(url = reporturl, headers = headers, data = element).text
+    #print(element2)
+    element2 = find_id(element2)
+    dimentionlist.append(element2)
+    return element2
+
 
